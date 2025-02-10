a7b96f0cf63c49cb36c02ccaf789d3315ff4e912
TriNHM <triminh0502@gmail.com>
2025-02-10 21:26:31 +0700

    [ADD] add authenticate account function

---
+++ SmartEnrol/SmartEnrol.Services/AccountSer/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using SmartEnrol.Services.ViewModels.Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartEnrol.Services.AccountSer
+{
+    public interface IAccountService
+    {
+        Task<(bool, string)> Authenticate(LoginModel login);
+    }
+}
