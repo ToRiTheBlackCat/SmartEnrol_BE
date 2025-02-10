320f451b4fab07115c2e6f186c9469c116e162ed
TriNHM <triminh0502@gmail.com>
2025-02-10 21:26:49 +0700

    [ADD] add new Roles constant enum

---
+++ SmartEnrol/SmartEnrol.Services/Constant/ConstantEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartEnrol.Services.Constant
+{
+    public static class ConstantEnum
+    {
+        public static class Roles
+        {
+            public const string ADMIN = "Admin";
+            public const string STUDENT = "Student";
+            public const string STAFF = "Staff";
+        }
+    }
+}
