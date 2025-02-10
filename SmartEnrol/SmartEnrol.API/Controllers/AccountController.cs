26e2dddc329bb7f7d59d374cbddda82be852560f
TriNHM <triminh0502@gmail.com>
2025-02-10 21:29:27 +0700

    [ADD] add new LoginWithEmailAndPassword api

---
+++ SmartEnrol/SmartEnrol.API/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartEnrol.Services.AccountSer;
+using SmartEnrol.Services.ViewModels.Student;
+
+namespace SmartEnrol.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        /// <summary>
+        /// Login account with email and password
+        /// LoginModel
+        /// Return isAuthenticated and token
+        /// </summary>
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginWithEmailAndPassword([FromBody] LoginModel request)
+        {
+            //Check required fields
+            if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Password))
+                return BadRequest();
+
+            //Authenticate account
+            var (isAuthenticated, response) = await _accountService.Authenticate(request);
+
+            return isAuthenticated 
+                ? Ok(new
+                    {
+                        Token = response
+                    }) 
+                : BadRequest(new
+                    {
+                        Token = response
+                    });
+        }
+    }
+}
