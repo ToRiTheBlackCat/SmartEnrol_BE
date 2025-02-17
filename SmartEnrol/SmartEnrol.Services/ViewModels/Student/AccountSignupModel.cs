043a2d83b015f88fc7eee15f840976e814306a95
TriNHM <triminh0502@gmail.com>
2025-02-17 13:53:17 +0700

    [UPDATE] update signup model

--- SmartEnrol/SmartEnrol.Services/ViewModels/Student/AccountSignupModel.cs
+++ SmartEnrol/SmartEnrol.Services/ViewModels/Student/AccountSignupModel.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 public class AccountSignupModel
 {
     [Required]
-    public string? AccountName { get; set; }
+    public string AccountName { get; set; } = string.Empty;
     [Required]
     [EmailAddress]
-    public string? Email { get; set; }
+    public string Email { get; set; } = string.Empty;
     [Required]
-    public string? Password { get; set; }
+    public string Password { get; set; } = string.Empty;
     [Required]
-    public string? ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
